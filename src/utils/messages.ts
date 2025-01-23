e36c928b1470e0024c3efbba42545e79aae73085
Renan Braga <azhariel@gmail.com>
2025-01-23 02:24:47 -0300

    Add message constants for ride statuses

---
+++ src/utils/messages.ts
@@ -0,0 +1,6 @@
+export const messages = {
+	default: 'Join us for the next ride!',
+	soon: 'The ride is starting soon! Get ready! 🚴‍♂️',
+	ongoing: 'The ride is happening right now!',
+	completed: 'Hope you enjoyed the ride! See you next time!',
+};
